7e5211e025bb45151d1d73efa4c93612a52e5512
Small, Jeff <Jeff.Small@capitalone.com>
2015-12-29 14:05:48 -0800

    Initial Commit

---
+++ MenuActivity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuActivity
+//
+//  Created by Small, Jeff on 12/29/15.
+//  Copyright © 2015 Small, Jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
